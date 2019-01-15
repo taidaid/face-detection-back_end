45396c98c4bcf7a5f75dc83851bf85480c4cfb5c
Bryan <windsor.bryan@gmail.com>
2019-01-15 19:41:23 +0100

    adding routes

--- server.js
+++ server.js
@@ -1,25 +1,106 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const bcrypt = require("bcrypt-nodejs");
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("Hello");
-  next();
-});
+const db = {
+  users: [
+    {
+      id: "123",
+      name: "John",
+      email: "john@gmail.com",
+      password: "cookies",
+      entries: 0,
+      joined: new Date()
+    },
+    {
+      id: "124",
+      name: "Sally",
+      email: "sally@gmail.com",
+      password: "bananas",
+      entries: 0,
+      joined: new Date()
+    }
+  ],
+  login: [
+    {
+      id: "123",
+      email: "john@gmail.com",
+      password: "cookies"
+    }
+  ]
+};
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
-  res.send("getting root");
+  console.log(req.params);
+  res.send(db.users);
 });
 
-app.get("/profile", (req, res) => {
-  res.send("getting profile");
+// ('/') => res = this is working
+
+// ('/signin') => POST = success/fail
+app.post("/signin", (req, res) => {
+  // Load hash from your password DB.
+  bcrypt.compare("bacon", hash, function(err, res) {
+    // res == true
+  });
+  bcrypt.compare("veggies", hash, function(err, res) {
+    // res = false
+  });
+  if (
+    req.body.email === db.users[0].email &&
+    req.body.password === db.users[0].password
+  ) {
+    res.json("success");
+  } else {
+    res.status(400).json("error logging in");
+  }
 });
 
-app.post("/", (req, res) => {
-  const user = {
-    name: "Sally",
-    hobby: "soccer"
-  };
-  res.send(user);
+// ('register') => POST = user
+app.post("/register", (req, res) => {
+  const { email, name, password } = req.body;
+  bcrypt.hash(password, null, null, function(err, hash) {
+    // Store hash in your password DB.
+    console.log(hash);
+  });
+  db.users.push({
+    id: "125",
+    name: name,
+    email: email,
+    password: password,
+    entries: 0,
+    joined: new Date()
+  });
+  res.json(db.users[db.users.length - 1]);
 });
-app.listen(3000);
+
+// ('profile/:userId') => GET = user
+app.get("/profile/:id", (req, res) => {
+  const { id } = req.params;
+  const user = db.users.filter(user => user.id === id);
+  if (user) {
+    return res.json(user);
+  } else {
+    return res.status(404).json("No such user");
+  }
+});
+
+// ('image') => PUT = user
+
+app.put("/image", (req, res) => {
+  const { id } = req.body;
+  const users = db.users.filter(user => user.id === id);
+  if (users.length === 1) {
+    users[0].entries++;
+    return res.json(users[0].entries);
+  } else {
+    return res.status(404).json("Error finding user");
+  }
+});
+
+app.listen(3000, () => console.log("app is listening on port 3000"));
