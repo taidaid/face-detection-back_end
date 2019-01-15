e32750d6fdc5779ad7dfdf7e2f23d17d2c72f8bf
Bryan <windsor.bryan@gmail.com>
2019-01-15 21:03:45 +0100

    adding sign in and register funcitons

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const bcrypt = require("bcrypt-nodejs");
+const cors = require("cors");
 
 const app = express();
 
@@ -34,9 +35,10 @@ const db = {
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
-  console.log(req.params);
+  console.log("root");
   res.send(db.users);
 });
 
@@ -44,30 +46,35 @@ app.get("/", (req, res) => {
 
 // ('/signin') => POST = success/fail
 app.post("/signin", (req, res) => {
+  console.log("sign in attempt");
   // Load hash from your password DB.
-  bcrypt.compare("bacon", hash, function(err, res) {
-    // res == true
-  });
-  bcrypt.compare("veggies", hash, function(err, res) {
-    // res = false
-  });
+  // bcrypt.compare("bacon", hash, function(err, res) {
+  //   // res == true
+  // });
+  // bcrypt.compare("veggies", hash, function(err, res) {
+  //   // res = false
+  // });
+
   if (
     req.body.email === db.users[0].email &&
     req.body.password === db.users[0].password
   ) {
-    res.json("success");
+    res.json(db.users[0]);
+    console.log("success");
   } else {
     res.status(400).json("error logging in");
+    console.log("error logging in");
   }
 });
 
 // ('register') => POST = user
 app.post("/register", (req, res) => {
+  console.log("register");
   const { email, name, password } = req.body;
-  bcrypt.hash(password, null, null, function(err, hash) {
-    // Store hash in your password DB.
-    console.log(hash);
-  });
+  // bcrypt.hash(password, null, null, function(err, hash) {
+  //   // Store hash in your password DB.
+  //   console.log(hash);
+  // });
   db.users.push({
     id: "125",
     name: name,
@@ -77,10 +84,13 @@ app.post("/register", (req, res) => {
     joined: new Date()
   });
   res.json(db.users[db.users.length - 1]);
+  console.log(db.users[db.users.length - 1]);
+  console.log("Successful Registration");
 });
 
 // ('profile/:userId') => GET = user
 app.get("/profile/:id", (req, res) => {
+  console.log("get profile");
   const { id } = req.params;
   const user = db.users.filter(user => user.id === id);
   if (user) {
